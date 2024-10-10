70979512d5b10ae7792f464438c3e2ae1fcb0661
Diego Gerwig <dgerwig-@student.42urduliz.com>
2024-10-10 13:06:48 +0200

    adapt json files

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from config import buyer_profiles
 from trx_generator import generate_trxs
 from utils import generate_spanish_dni, generate_email, generate_password
+import random
 
 
 def generate_data():
@@ -12,10 +13,15 @@ def generate_data():
     transactions = []
     start_date = datetime.strptime("2022-01-01", "%Y-%m-%d")
 
+    print(f"\nGenerating data for {len(buyer_profiles)} profiles:")
+    print("-" * 50)
+
     for profile_name, profile_data in buyer_profiles.items():
         dni = generate_spanish_dni()
         email = generate_email(profile_data['name'], profile_data['surname'])
         password = generate_password(6)
+
+        initial_assets = round(float(random.uniform(*profile_data['initial_assets'])), 2)
         
         user = {
             "profile": profile_name,
@@ -27,7 +33,7 @@ def generate_data():
             "password": password,
             "city": profile_data['city'],
             "iban": profile_data['iban'],
-            "balance": 0
+            "assets": initial_assets,
         }
         users.append(user)
         
@@ -36,14 +42,14 @@ def generate_data():
         for trx in data['trxs']:
             transaction = {
                 "dni": dni,
+                "type": "expenses" if trx['amount_eur'] < 0 else "incomes",
                 "category": trx['trx_cat'],
                 "amount": trx['amount_eur'],
-                "timestamp": {"$date": trx['timestamp']}
+                "timestamp": trx['timestamp']
             }
             transactions.append(transaction)
-            
-            # Update user balance
-            user['balance'] += trx['amount_eur']
+        
+        print(f"💥 Total transactions for {user['profile']}: \t{len(data['trxs'])}")
 
     return users, transactions
 
@@ -56,44 +62,48 @@ def save_json(data, filename):
 def save_csv(users, transactions, filename):
     with open(filename, 'w', newline='', encoding='utf-8') as f:
         writer = csv.writer(f)
-        writer.writerow(['profile', 'name', 'surname', 'birth_date', 'dni', 'email', 'password', 'city', 'iban', 'balance', 'category', 'amount', 'timestamp'])
+        writer.writerow(['profile', 'name', 'surname', 'birth_date', 'dni', 'email', 'password', 'iban', 'assets', 'timestamp', 'city', 'type', 'category', 'amount', 'balance'])
         
         for user in users:
             user_transactions = [t for t in transactions if t['dni'] == user['dni']]
+            balance = round(user['assets'], 2)  # Initialize balance with the initial assets value, rounded to 2 decimal places
             if user_transactions:
                 for transaction in user_transactions:
+                    balance = round(balance + transaction['amount'], 2)  # Update balance before writing the row, round to 2 decimal places
                     writer.writerow([
                         user['profile'], user['name'], user['surname'], user['birth_date'],
-                        user['dni'], user['email'], user['password'], user['city'], user['iban'], user['balance'],
-                        transaction['category'], transaction['amount'], transaction['timestamp']['$date']
+                        user['dni'], user['email'], user['password'], user['iban'], user['assets'],
+                        transaction['timestamp'], user['city'], 
+                        transaction['type'], transaction['category'], transaction['amount'],
+                        balance
                     ])
             else:
                 writer.writerow([
                     user['profile'], user['name'], user['surname'], user['birth_date'],
-                    user['dni'], user['email'], user['password'], user['city'], user['iban'], user['balance'],
-                    '', '', ''
+                    user['dni'], user['email'], user['password'], user['iban'], user['assets'],
+                    '', user['city'], '', '', '', balance
                 ])
 
 
 if __name__ == "__main__":
     users, transactions = generate_data()
 
-    # Crear carpeta de datos fuera de src
+    # Create data folder outside of src
     project_root = Path(__file__).resolve().parent.parent
     data_folder = project_root / "data"
     data_folder.mkdir(exist_ok=True)
 
-    # Guardar archivos JSON
+    # Save JSON files
     users_json_path = data_folder / "userInsert.json"
     transactions_json_path = data_folder / "transactionInsert.json"
     save_json(users, users_json_path)
     save_json(transactions, transactions_json_path)
 
-    # Guardar archivo CSV
+    # Save CSV file
     csv_path = data_folder / "data_bank_trx.csv"
     save_csv(users, transactions, csv_path)
 
-    # Generar output final
+    # Generate final output
     try:
         users_json_relative_path = users_json_path.relative_to(project_root)
         transactions_json_relative_path = transactions_json_path.relative_to(project_root)
@@ -101,9 +111,9 @@ if __name__ == "__main__":
         
         print(f"\n✅ Total users generated: \t\t{len(users)}")
         print(f"✅ Total transactions generated: \t{len(transactions)}")
-        print(f"✅ Users         JSON file saved at: {users_json_relative_path}")
-        print(f"✅ Transactions  JSON file saved at: {transactions_json_relative_path}")
-        print(f"✅ Data          CSV  file saved at: {csv_relative_path}")
+        print(f"✅ Users         JSON file saved at: \t{users_json_relative_path}")
+        print(f"✅ Transactions  JSON file saved at: \t{transactions_json_relative_path}")
+        print(f"✅ Data          CSV  file saved at: \t{csv_relative_path}")
     except ValueError as e:
         print(f"\n❌ Error calculating relative paths: {e}")
         print(f"✅ Total users generated: {len(users)}")
